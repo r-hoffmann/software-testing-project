7f8eba99f9fe85a39ca88f5089900390329377b9
Roland <roland@kasplan.nl>
2019-04-29 13:21:00 +0200

    update tests to use new guessed_character way

--- hangman_unittest.py
+++ hangman_unittest.py
@@ -1,4 +1,4 @@
-import containers, io, string, unittest, unittest.mock
+import containers, io, mock, string, unittest, sys
 from hangman_beta import Hangman
 
 class TestHangman(unittest.TestCase):
@@ -18,20 +18,23 @@ class TestHangman(unittest.TestCase):
 
     def test_enter_one_letter(self):
         hangman = Hangman()
-        self.assertFalse(hangman.validUserInput('aa'))
+        hangman.guessed_character = 'aa'
+        self.assertFalse(hangman.validUserInput())
         
     def test_letter_correct(self):
         hangman = Hangman()
-        for s in string.printable:
-            valid = hangman.validUserInput(s)
-            self.assertEqual(valid, s in string.ascii_lowercase)
+        for character in string.printable:
+            hangman.guessed_character = character
+            valid = hangman.validUserInput()
+            self.assertEqual(valid, character in string.ascii_uppercase)
         
     def test_incorrect_letter_changes_hangman(self):
         hangman = Hangman(False, 9, 'hangman')
         hangman_figures = []
         for character in 'bcdefijkl':
             hangman_figures.append(hangman.getHangman())
-            hangman.processInput(character)
+            hangman.guessed_character = character
+            hangman.processInput()
 
         for figure_1, figure_2 in zip(hangman_figures[:-1], hangman_figures[1:]):
             self.assertNotEqual(figure_1, figure_2)
@@ -42,7 +45,8 @@ class TestHangman(unittest.TestCase):
         lives = hangman.lives
         for character in 'bcdefijkl':
             hangman_figures.append(hangman.getHangman())
-            hangman.processInput(character)
+            hangman.guessed_character = character
+            hangman.processInput()
             self.assertTrue(hangman.lives < lives)
             lives = hangman.lives
         
@@ -52,46 +56,47 @@ class TestHangman(unittest.TestCase):
     def test_user_completes_word_implies_win(self):
         hangman = Hangman(False, 9, 'hangman')
         for character in 'hangm':
-            hangman.processInput(character)
+            hangman.guessed_character = character
+            hangman.processInput()
         self.assertTrue(hangman.finished)
         self.assertTrue(hangman.won)
         
     def test_user_without_lives_implies_loss(self):
         hangman = Hangman(False, 9, 'hangman')
         for character in 'bcdefijkl':
-            hangman.processInput(character)
+            hangman.guessed_character = character
+            hangman.processInput()
         self.assertTrue(hangman.finished)
         self.assertFalse(hangman.won)
         
     def test_supply_feedback(self):
-        pass
+        for character in string.ascii_letters:
+            with mock.patch('builtins.input', return_value=character):
+                hangman = Hangman()
+                assert character.upper() in hangman.turn()
         
     def test_user_guesses_last_character(self):
         pass
         
     def test_user_enters_invalid_character(self):
         pass
-        
-    def test_user_enters_punctuation(self):
-        # see test_letter_correct
-        pass
-
-    def test_user_enters_number(self):
-        # see test_letter_correct
-        pass
 
     def test_user_enters_multiple_characters(self):
         hangman = Hangman()
-        self.assertFalse(hangman.validUserInput('aa'))
+        hangman.guessed_character = 'aa'
+        self.assertFalse(hangman.validUserInput())
         
     def test_user_enters_no_character(self):
         hangman = Hangman()
-        self.assertFalse(hangman.validUserInput(''))
+        hangman.guessed_character = ''
+        self.assertFalse(hangman.validUserInput())
         
     def test_user_enters_same_character_multiple_times(self):
         hangman = Hangman()
-        hangman.processInput('a')
-        self.assertTrue(hangman.characterAlreadyGuessed('a'))
+        hangman.guessed_character = 'a'
+        hangman.processInput()
+        hangman.guessed_character = 'a'
+        self.assertTrue(hangman.characterAlreadyGuessed())
         
 if __name__ == '__main__':
     unittest.main()
